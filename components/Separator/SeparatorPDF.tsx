0ecdfa6e971fccfb23bfb747d533f27e24df54ff
OmkoBass <omkob@hotmail.com>
2022-04-03 22:37:41 +0200

    add props for easier customization

--- components/Separator/SeparatorPDF.tsx
+++ components/Separator/SeparatorPDF.tsx
@@ -2,14 +2,19 @@ import React from "react";
 
 import { View } from "@react-pdf/renderer";
 
-export const SeparatorPDF = () => {
-  return (
-    <View
-      style={{
-        margin: "0 10 0 10",
-        borderBottomColor: "#e7e7e7",
-        borderBottomWidth: 1,
-      }}
-    />
-  );
+interface Props {
+  style?: object;
+  color?: string;
+  borderWidth?: number;
+}
+
+export const SeparatorPDF: React.FC<Props> = ({ style, color, borderWidth }) => {
+  const originalStyles = {
+    borderBottomColor: color || "#e7e7e7",
+    borderBottomWidth: borderWidth || 4,
+  };
+
+  const combinedStyles = { ...style, ...originalStyles };
+
+  return <View style={combinedStyles} />;
 };
